0b1b92190d8d812247ca04e6b52bfbbb265506db
johncholo <johncholokahulugan08@gmail.com>
2024-06-30 16:12:17 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,62 @@
+let songs = [
+    {
+       name: 'Tingin',
+       path: 'songs/tingin.mp3',
+       artist: 'Cup of Joe & Janine Teñoso',
+       cover: 'images/tingin.jpg',
+    },
+    {
+       name: 'Heaven Knows',
+       path: 'songs/ol.mp3',
+       artist: 'Orange & Lemon',
+       cover: 'images/ol.jpg',
+    },
+    {
+       name: 'Migraine',
+       path: 'songs/moon.mp3',
+       artist: 'Moonstar 88',
+       cover: 'images/moon.jpg',
+    },
+    {
+       name: 'Tanga mo Juan',
+       path: 'songs/jk.mp3',
+       artist: 'Juan Karlos',
+       cover: 'images/jk.jpg',
+    },
+    {
+       name: 'Alam mo ba girl',
+       path: 'songs/hev.mp3',
+       artist: 'Hev Avi',
+       cover: 'images/hev.jpg',
+    },
+    {
+      name: 'Dilaw',
+      path: 'songs/dil.mp3',
+      artist: 'Maki',
+      cover: 'images/dil.jpg',
+   },
+   {
+      name: 'Girl be mine',
+      path: 'songs/fm.mp3',
+      artist: 'Francis M',
+      cover: 'images/fm.jpg'
+   },
+   {
+      name: 'Kaleidoscope World',
+      path: 'songs/pne.mp3',
+      artist: 'PnE & Francis M',
+      cover: 'images/pne.jpg',
+   },
+   {
+      name: 'Better Days',
+      path: 'songs/franco.mp3',
+      artist: 'Franco',
+      cover: 'images/franco.jpg',
+   },
+   {
+      name: 'Kahit di mo alam',
+      path: 'songs/da.mp3',
+      artist: 'December Avenue',
+      cover: 'images/da.jpg',
+   }
+   ]
